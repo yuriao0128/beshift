099ed296f98ffbbc4b459e64578e4bc384c6957b
yuriao0128 <yuriao@outlook.jp>
2025-03-29 20:42:27 +0900

    admin.loginルート削除のためミドルウェア設定修正

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // $middleware->redirectGuestsTo(fn() =>route('admin.login'));
         $middleware->redirectUsersTo(fn() =>route('users.mypage'));
 
     })
